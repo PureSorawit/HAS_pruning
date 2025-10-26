e1b3f0462e435c20f6bb18e5b4900c2faa73240d
Sorawit Ongsiriporn <puresorawit@gmail.com>
2025-10-26 15:56:54 +0100

    fixed download path

--- download_model.py
+++ download_model.py
@@ -6,7 +6,14 @@ def get_vit_base_cifar100(save_path: str = "./checkpoints/vit_base_cifar100.pth"
     os.makedirs(os.path.dirname(save_path), exist_ok=True)
 
     # Create the model
-    model = timm.create_model("vit_base_patch16_224", pretrained=True, num_classes=100)
+    # model = timm.create_model("vit_base_patch16_224", pretrained=True, num_classes=100)
+    # torch.save(model.state_dict(), save_path)
+
+    url = "https://huggingface.co/edadaltocg/vit_base_patch16_224_in21k_ft_cifar100/resolve/main/pytorch_model.bin"
+    state = torch.hub.load_state_dict_from_url(url, map_location="cpu", file_name="vit_base_cifar100.pth")
+
+    model = timm.create_model("vit_base_patch16_224", pretrained=False, num_classes=100)
+    model.load_state_dict(state, strict=False)
     torch.save(model.state_dict(), save_path)
 
     print(f"Pretrained ViT-B/16 saved at: {save_path}")
